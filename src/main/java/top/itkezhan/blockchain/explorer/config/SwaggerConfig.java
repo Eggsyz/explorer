92ff7c22fb52eb53d0c90ea5a6738e4adc6227e1
Eggsyz <liguo_ji@outlook.com>
2020-12-24 08:57:14 +0800

    add: 新增通用模块代码以及swagger配置

---
+++ src/main/java/top/itkezhan/blockchain/explorer/config/SwaggerConfig.java
@@ -0,0 +1,7 @@
+package top.itkezhan.blockchain.explorer.config;/**
+ * @Classname SwaggerConfig
+ * @Description TODO
+ * @Date 2020/12/24 8:37
+ * @Created by JILIGUO
+ */public class SwaggerConfig {
+}
